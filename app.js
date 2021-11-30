4af8fdfeee4c0ef48e09ffa8d8c83545d233054b
AntonyNest <34522785+AntonyNest@users.noreply.github.com>
2021-11-30 23:07:15 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+const board = document.querySelector('#board')
+const colors = ['#e0b0b0', '#8e44ad', '#7ba5b2', 'rgba(230,126,32,0.84)', '#e54c3c','#2ecc78']
+const SQUARES_NUMBER = 1800
+
+for (let i = 0; i < SQUARES_NUMBER; i++) {
+    const square = document.createElement('div')
+    square.classList.add('square')
+
+    square.addEventListener('mouseover', () => setColor(square))
+
+    square.addEventListener('mouseleave', () => removeColor(square))
+
+    board.append(square)
+}
+
+function setColor(element) {
+    const color = getRandomColor()
+    element.style.backgroundColor = color
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+}
+
+function removeColor(element) {
+    element.style.backgroundColor = '#1d1d1d'
+    element.style.boxShadow = `0 0 2px #000`
+}
+
+function getRandomColor() {
+    const index = Math.floor(Math.random() * colors.length)
+    return colors[index]
+}
